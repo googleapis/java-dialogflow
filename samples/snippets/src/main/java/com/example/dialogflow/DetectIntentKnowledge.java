3d58e8eabb1258ae6e5cd307b20356fd86884dfe
b-loved-dreamer <franklinsnunez@gmail.com>
2021-03-31 14:58:09 -0700

    samples: updated legacy field

--- samples/snippets/src/main/java/com/example/dialogflow/DetectIntentKnowledge.java
+++ samples/snippets/src/main/java/com/example/dialogflow/DetectIntentKnowledge.java
@@ -81,7 +81,7 @@ public class DetectIntentKnowledge {
             "Detected Intent: %s (confidence: %f)\n",
             queryResult.getIntent().getDisplayName(), queryResult.getIntentDetectionConfidence());
         System.out.format(
-            "Fulfillment Text: '%s'\n", queryResult.getFulfillmentMessages(0).getText());
+            "Fulfillment Text: '%s'\n", queryResult.getFulfillmentText());
         KnowledgeAnswers knowledgeAnswers = queryResult.getKnowledgeAnswers();
         for (Answer answer : knowledgeAnswers.getAnswersList()) {
           System.out.format(" - Answer: '%s'\n", answer.getAnswer());
