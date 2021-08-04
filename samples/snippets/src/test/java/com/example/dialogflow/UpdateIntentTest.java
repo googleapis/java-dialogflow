ce287de9a25d5500b13f5d1dab48d2591d023941
Gal Zahavi <galzahavi@google.com>
2021-08-04 16:16:17 -0700

    Fixed spacing

--- samples/snippets/src/test/java/com/example/dialogflow/UpdateIntentTest.java
+++ samples/snippets/src/test/java/com/example/dialogflow/UpdateIntentTest.java
@@ -19,7 +19,6 @@ package com.example.dialogflow;
 import com.google.cloud.dialogflow.v2.AgentName;
 import com.google.cloud.dialogflow.v2.Intent;
 import com.google.cloud.dialogflow.v2.IntentsClient;
-
 import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Before;
