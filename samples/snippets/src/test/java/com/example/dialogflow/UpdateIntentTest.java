641f7d6b00d969d79ded3bf33be0542009fa3500
Gal Zahavi <galzahavi@google.com>
2021-08-04 16:15:03 -0700

    Lint fix

--- samples/snippets/src/test/java/com/example/dialogflow/UpdateIntentTest.java
+++ samples/snippets/src/test/java/com/example/dialogflow/UpdateIntentTest.java
@@ -21,7 +21,6 @@ import com.google.cloud.dialogflow.v2.Intent;
 import com.google.cloud.dialogflow.v2.IntentsClient;
 
 import java.io.IOException;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
