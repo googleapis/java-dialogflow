6953249d619548f67339c38181f73748e3e01fcf
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2021-09-09 12:34:13 -0700

    diversify Action name

--- google-cloud-dialogflow/src/test/java/com/google/cloud/dialogflow/v2/it/ITSystemTest.java
+++ google-cloud-dialogflow/src/test/java/com/google/cloud/dialogflow/v2/it/ITSystemTest.java
@@ -54,13 +54,13 @@ import com.google.cloud.dialogflow.v2.SessionsClient;
 import com.google.protobuf.Struct;
 import com.google.protobuf.Value;
 import java.io.IOException;
+import java.util.Random;
 import java.util.UUID;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ITSystemTest {
-
   private static AgentsClient agentsClient;
   private static ContextsClient contextsClient;
   private static Context context;
@@ -87,7 +87,9 @@ public class ITSystemTest {
       EntityType.Entity.newBuilder().addSynonyms("test-entity").setValue("test-entity").build();
   private static final String INTENT_NAME = "test-intent-" + ID;
   private static final String EVENT_NAME = "test-event";
-  private static final String ACTION_NAME = "test-action";
+  private static Random rn = new Random();
+  private static final String[] ACTION_NAME_ARRAY = {"test-action1","test-action2","test-action3","test-action4","test-action5"};
+    private static final String ACTION_NAME = ACTION_NAME_ARRAY[rn.nextInt(5)];
   private static final String SESSION_ID = UUID.randomUUID().toString();
   private static final SessionName SESSION_NAME = SessionName.of(PROJECT_ID, SESSION_ID);
   private static final String CONTEXT_ID = "test-context-" + ID;
