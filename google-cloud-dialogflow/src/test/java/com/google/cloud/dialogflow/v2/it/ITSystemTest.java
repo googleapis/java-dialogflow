9b4d4f8be71f96eb10f72f7294126a8faf7bd2e0
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2021-09-09 12:40:13 -0700

    fix test

--- google-cloud-dialogflow/src/test/java/com/google/cloud/dialogflow/v2/it/ITSystemTest.java
+++ google-cloud-dialogflow/src/test/java/com/google/cloud/dialogflow/v2/it/ITSystemTest.java
@@ -89,7 +89,7 @@ public class ITSystemTest {
   private static final String EVENT_NAME = "test-event";
   private static Random rn = new Random();
   private static final String[] ACTION_NAME_ARRAY = {"test-action1","test-action2","test-action3","test-action4","test-action5"};
-    private static final String ACTION_NAME = ACTION_NAME_ARRAY[rn.nextInt(5)];
+  private static final String ACTION_NAME = "test-action";
   private static final String SESSION_ID = UUID.randomUUID().toString();
   private static final SessionName SESSION_NAME = SessionName.of(PROJECT_ID, SESSION_ID);
   private static final String CONTEXT_ID = "test-context-" + ID;
