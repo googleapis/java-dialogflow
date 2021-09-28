9b1147d1121607469926d8eeef2f171a9c159cbe
Gal Zahavi <galzahavi@google.com>
2021-09-28 11:17:14 -0700

    Changed Test

--- samples/snippets/src/test/dialogflow/ExampleIT.java
+++ samples/snippets/src/test/dialogflow/ExampleIT.java
@@ -14,56 +14,64 @@
  * limitations under the License.
  */
 
-package dialogflow;
+package dialogflow.cx;
 
 import static com.google.common.truth.Truth.assertThat;
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.mockito.Mockito.when;
 
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import com.google.gson.Gson;
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Map;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+
 
 public class ExampleIT {
+  @Mock private HttpRequest request;
+  @Mock private HttpResponse response;
 
-    private ByteArrayOutputStream stdOut;
-
-    private static BufferedReader getRequestJSONReader() {
-    byte[] b =
-        ("{
-            \"queryResult\": {
-              \"intent\": {
-                \"name\": \"projects/galstarter-316823/agent/intents/00c2877d-2440-447f-8dc1-045623a55bd4\",
-                \"displayName\": \"Default Welcome Intent\"
-              },
-            },
-          }")
-            .getBytes(UTF_8);
-    InputStream is = new ByteArrayInputStream(b);
-    return new BufferedReader(new InputStreamReader(is, UTF_8));
-  }
-
+  private BufferedWriter writerOut;
+  private StringWriter responseOut;
+  private static final Gson gson = new Gson();
 
   @Before
-  public void setUp() throws IOException {
+  public void beforeTest() throws IOException {
+    MockitoAnnotations.initMocks(this);
 
-    stdOut = new ByteArrayOutputStream();
-    System.setOut(new PrintStream(stdOut));
+    // use an empty string as the default request content
+    BufferedReader reader = new BufferedReader(new StringReader(""));
+    when(request.getReader()).thenReturn(reader);
+
+    responseOut = new StringWriter();
+    writerOut = new BufferedWriter(responseOut);
+    when(response.getWriter()).thenReturn(writerOut);
   }
 
   @Test
-  public void testWebhook() throws IOException {
-    when(request.getReader()).thenReturn(getRequestJSONReader());
-    Example.service()
-    assertThat(stdOut.toString()).contains("Hello from a Java GCF Webhook");
+  public void helloHttp_bodyParamsPost() throws IOException {
+    BufferedReader jsonReader = new BufferedReader(new StringReader("{
+      \"queryResult\": {
+        \"intent\": {
+          \"name\": \"projects/\",
+          \"displayName\": \"Default Welcome Intent\"
+        },
+      },
+    }"));
 
+    when(request.getReader()).thenReturn(jsonReader);
+
+    new Webhook().service(request, response);
+    writerOut.flush();
+
+    assertThat(responseOut.toString()).contains("Hello from a Java GCF Webhook");
   }
 }
