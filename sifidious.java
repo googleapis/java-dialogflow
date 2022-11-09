96c25a2bd3547604ece8dc0a84f09a826aa97863
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2022-11-09 00:11:30 -0600

    Create sifidious.java
    Created sifidious.java for dev branch

---
+++ sifidious.java
@@ -0,0 +1,47 @@
+  public static void main(String args[])
+    {
+        X3D thisExampleX3dModel = new Caffeine().getX3dModel();
+
+		boolean hasArguments = (args != null) && (args.length > 0);
+		boolean validate = true; // default
+		boolean argumentsLoadNewModel = false;
+		String  fileName = new String();
+
+		if (args != null)
+		{
+			for (String arg : args)
+			{
+				if (arg.toLowerCase().startsWith("-v") || arg.toLowerCase().contains("validate"))
+				{
+					validate = true; // making sure
+				}
+				if (arg.toLowerCase().endsWith(X3D.FILE_EXTENSION_X3D) ||
+					arg.toLowerCase().endsWith(X3D.FILE_EXTENSION_CLASSICVRML) ||
+					arg.toLowerCase().endsWith(X3D.FILE_EXTENSION_X3DB) ||
+					arg.toLowerCase().endsWith(X3D.FILE_EXTENSION_VRML97) ||
+					arg.toLowerCase().endsWith(X3D.FILE_EXTENSION_EXI) ||
+					arg.toLowerCase().endsWith(X3D.FILE_EXTENSION_GZIP) ||
+					arg.toLowerCase().endsWith(X3D.FILE_EXTENSION_ZIP) ||
+					arg.toLowerCase().endsWith(X3D.FILE_EXTENSION_HTML) ||
+					arg.toLowerCase().endsWith(X3D.FILE_EXTENSION_XHTML))
+				{
+					argumentsLoadNewModel = true;
+					fileName = arg;
+				}
+			}
+		}
+		if      (argumentsLoadNewModel)
+			System.out.println("WARNING: \"Basic.ChemicalMarkupLanguage.Caffeine\" model invocation is attempting to load file \"" + fileName + "\" instead of simply validating itself... file loading ignored.");
+		else if (hasArguments) // if no arguments provided, this method produces usage warning
+			thisExampleX3dModel.handleArguments(args);
+
+		if (validate)
+		{
+			System.out.print("Java program \"Basic.ChemicalMarkupLanguage.Caffeine\" self-validation test results: ");
+			String validationResults = thisExampleX3dModel.validationReport();
+            if (validationResults.startsWith("\n"))
+                System.out.println();
+			System.out.println(validationResults.trim());
+		}
+    }
+}
